1d0e37e5e5baabd9e9d27755bb6359f73dc6a3d6
Timi Delad <timilehinogunlade@gmail.com>
2024-10-08 04:22:06 -0700

    added essay page

--- public/script.js
+++ public/script.js
@@ -1,3 +1,42 @@
+window.onload = function() {
+    document.getElementById('poetry-section').classList.remove('hidden');
+    document.getElementById('storyTelling-section').classList.add('hidden');
+    document.getElementById('contentWriting-section').classList.add('hidden');
+    document.getElementById('essay-section').classList.add('hidden');
+};
+
+function displayContent(event) {
+    event.preventDefault();
+    document.getElementById('contentWriting-section').classList.remove('hidden');
+    document.getElementById('storyTelling-section').classList.add('hidden');
+    document.getElementById('poetry-section').classList.add('hidden');
+    document.getElementById('essay-section').classList.add('hidden');
+}
+
+function displayPoetry(event) {
+    event.preventDefault();
+    document.getElementById('poetry-section').classList.remove('hidden');
+    document.getElementById('contentWriting-section').classList.add('hidden');
+    document.getElementById('storyTelling-section').classList.add('hidden');
+    document.getElementById('essay-section').classList.add('hidden');
+}
+
+function displayStory(event) {
+    event.preventDefault();
+    document.getElementById('storyTelling-section').classList.remove('hidden');
+    document.getElementById('poetry-section').classList.add('hidden');
+    document.getElementById('contentWriting-section').classList.add('hidden');
+    document.getElementById('essay-section').classList.add('hidden');
+}
+
+function displayEssay(event) {
+    event.preventDefault();
+    document.getElementById('essay-section').classList.remove('hidden');
+    document.getElementById('contentWriting-section').classList.add('hidden');
+    document.getElementById('storyTelling-section').classList.add('hidden');
+    document.getElementById('poetry-section').classList.add('hidden');
+}
+
 let navLink = document.getElementById('nav-Links');
 let navIcon = document.getElementById('navIcon');
 
@@ -23,7 +62,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 }).addTo(map);
 
-L.marker([7.6211, 5.2214]).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([7.6211, 5.2214]).addTo(map).bindPopup('')
 
 
 const contactForm = document.getElementById('contactForm');
@@ -32,7 +71,6 @@ const contactForm = document.getElementById('contactForm');
 contactForm.addEventListener('submit', function(e) {
     e.preventDefault();  
 
-    // Collect form data
     const formData = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
@@ -40,7 +78,6 @@ contactForm.addEventListener('submit', function(e) {
         message: document.getElementById('message').value
     };
 
-    // Use EmailJS to send form data
     emailjs.send('service_q8z51z3', 'template_99n6em2', formData)
     .then(function(response) {
         alert("Message sent successfully! \u{1F389}");
